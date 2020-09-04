d800ddb6c7024c5c17e5b01222e6abf07ead6a23
Javier Esmith Blanco Ch <javieresmithblanco@gmail.com>
2020-09-04 16:53:20 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,90 +1,119 @@
+var API_URL = "https://api.le-systeme-solaire.net/rest/bodies/"
+var information = {"recently":"","status":"error","mass":0,"volume":0,"gravity":0}
+
+function get_api_data(name){
+	var request = new XMLHttpRequest()
+
+	if(information.recently!=name){
+		request.open('GET', API_URL+name, true)
+		request.onload = function () {
+
+		  var data = JSON.parse(this.response)
+
+		  if (request.status==200) {
+		  	information["recently"] = name
+		  	information["status"] = "successful"
+		  	information["mass"] = data.mass.massValue+" "+data.mass.massExponent
+		  	information["volume"] = data.vol.volValue+" "+data.vol.volExponent
+		  	information["gravity"] = data.gravity
+		  }
+		}
+		request.send()
+	}
+}
+
+function set_api_data(name, mass, volume, gravity){
+	if (information.status=="successful") {
+		planeta_text.setAttribute('value',name)
+		masa_text.setAttribute('value',"Masa: "+information.mass+" kg")
+		volumen_text.setAttribute('value',"Volumen: "+information.volume+" km3")
+		gravedad_text.setAttribute('value',"Gravedad: "+information.gravity+" m/s2")
+	}else{
+		planeta_text.setAttribute('value',name)
+		masa_text.setAttribute('value',"Masa: "+mass+" kg")
+		volumen_text.setAttribute('value',"Volumen: "+volume+" km3")
+		gravedad_text.setAttribute('value',"Gravedad: "+gravity+" m/s2")
+	}
+}
+
 AFRAME.registerComponent('do-something', {
   init: function () {
-    var sceneEl = this.el;
-    var datos = sceneEl.querySelector('#datos');
-    var planeta_text = sceneEl.querySelector('#planeta_text');
-    var masa_text = sceneEl.querySelector('#masa_text');
-    var volumen_text = sceneEl.querySelector('#volumen_text');
-    var gravedad_text = sceneEl.querySelector('#gravedad_text');
-    var mercurio = sceneEl.querySelector('#mercurio');
-    var venus = sceneEl.querySelector('#venus');
-    var tierra = sceneEl.querySelector('#tierra');
-    var marte = sceneEl.querySelector('#marte');
-    var jupiter = sceneEl.querySelector('#jupiter');
-    var saturno = sceneEl.querySelector('#saturno');
-    var urano = sceneEl.querySelector('#urano');
-    var neptuno = sceneEl.querySelector('#neptuno');
+    var sceneEl = this.el
+    var datos = sceneEl.querySelector('#datos')
+    var planeta_text = sceneEl.querySelector('#planeta_text')
+    var masa_text = sceneEl.querySelector('#masa_text')
+    var volumen_text = sceneEl.querySelector('#volumen_text')
+    var gravedad_text = sceneEl.querySelector('#gravedad_text')
+    var mercurio = sceneEl.querySelector('#mercurio')
+    var venus = sceneEl.querySelector('#venus')
+    var tierra = sceneEl.querySelector('#tierra')
+    var marte = sceneEl.querySelector('#marte')
+    var jupiter = sceneEl.querySelector('#jupiter')
+    var saturno = sceneEl.querySelector('#saturno')
+    var urano = sceneEl.querySelector('#urano')
+    var neptuno = sceneEl.querySelector('#neptuno')
 
     mercurio.addEventListener('click', function (evt) {
-  		planeta_text.setAttribute('value',"Mercurio");
-	    masa_text.setAttribute('value',"Masa: 3,302×10 23 kg");
-	    volumen_text.setAttribute('value',"Volumen: 6,083×10 10 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 3,7 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+    	var name = "Mercury"
+    	get_api_data(name)
+    	setTimeout( () => {set_api_data("Mercurio","3,302×10 23","6,083×10 10","3,7")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
   	});
 
   	venus.addEventListener('click', function (evt) {
-  		planeta_text.setAttribute('value',"Venus");
-	    masa_text.setAttribute('value',"Masa: 4,869×10 24 kg");
-	    volumen_text.setAttribute('value',"Volumen: 9,28x10 11 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 8,87 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+  		var name = "Venus"
+  		setTimeout( () => {set_api_data("Venus","4,869×10 24","9,28x10 11","8,87")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
   	});
 
     tierra.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Tierra");
-	    masa_text.setAttribute('value',"Masa: 5,9736×10 24 kg");
-	    volumen_text.setAttribute('value',"Volumen: 1,08321×10 12 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 9,780327 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+    	var name = "Earth"
+    	get_api_data(name)
+    	setTimeout( () => {set_api_data("Tierra","5,9736×10 24","1,08321×10 12","9,780327")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
 	marte.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Marte");
-	    masa_text.setAttribute('value',"Masa: 6,4185×10 23 kg");
-	    volumen_text.setAttribute('value',"Volumen: 1,6318×10 11 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 3,711 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+	    var name = "Mars"
+    	get_api_data(name)
+    	setTimeout( () => {set_api_data("Marte","6,4185×10 23","1,6318×10 11","3,711")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
 	jupiter.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Jupiter");
-	    masa_text.setAttribute('value',"Masa: 1,899×10 27 kg");
-	    volumen_text.setAttribute('value',"Volumen: 1,4313×10 15 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 24.79 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+		var name = "Jupiter"
+		get_api_data(name)
+    	setTimeout( () => {set_api_data("Jupiter","1,899×10 27","1,4313×10 15","24.79")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
 	saturno.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Saturno");
-	    masa_text.setAttribute('value',"Masa: 5,688x10 26 kg");
-	    volumen_text.setAttribute('value',"Volumen: 8,27x10 23 m3");
-	    gravedad_text.setAttribute('value',"Gravedad: 10,44 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+		var name = "Saturn"
+		get_api_data(name)
+    	setTimeout( () => {set_api_data("Saturno","5,688x10 26","8,27x10 23","10,44")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
 	urano.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Urano");
-	    masa_text.setAttribute('value',"Masa: 8,686×10 25 kg");
-	    volumen_text.setAttribute('value',"Volumen: 6,833×10 13 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 8,69 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+		var name = "Uranus"
+		get_api_data(name)
+    	setTimeout( () => {set_api_data("Urano","8,686×10 25","6,833×10 13","8,69")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
 	neptuno.addEventListener('click', function (evt) {
-	    planeta_text.setAttribute('value',"Neptuno");
-	    masa_text.setAttribute('value',"Masa: 1,024×10 26 kg3​");
-	    volumen_text.setAttribute('value',"Volumen: 6,254×1013 km3");
-	    gravedad_text.setAttribute('value',"Gravedad: 11,15 m/s2");
-	    datos.object3D.visible = true;
-	    setTimeout( () => {datos.object3D.visible = false;}, 8000);
+		var name = "Neptune"
+		get_api_data(name)
+    	setTimeout( () => {set_api_data("Neptuno","1,024×10 26","6,254×10 13","11,15")}, 1000)
+	    datos.object3D.visible = true
+	    setTimeout( () => {datos.object3D.visible = false}, 8000)
 	});
 
   	}
